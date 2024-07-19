20c11c10dfd0c51585fed9d30f12e1ec6216cee4
Yul-sketch <hayul.kim29@stu.siskorea.org>
2024-07-19 11:56:46 +0900

    Update header.py

--- header.py
+++ header.py
@@ -1,1 +1,7 @@
+import streamlit as st
+
+def create_header():
+  st.title("Heart Disease")     # YOUR CODE HERE
+  st.header("Input your heart rate and blood pressure")    # YOUR CODE HERE
+  st.subheader("Hello") # YOUR CODE HERE
 
