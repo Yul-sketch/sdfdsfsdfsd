72e9acef6d7648e1d51017feefbb0481c67705bd
Yul-sketch <hayul.kim29@stu.siskorea.org>
2024-07-19 11:57:55 +0900

    Update response.py

--- response.py
+++ response.py
@@ -1,1 +1,9 @@
+import streamlit as st
+
+def get_app_response(prediction):
+  if prediction == 1: # CHANGE THIS!
+    st.write("The model predicts you have heart disease")
+  # ADD MORE IF / ELIF STATEMENTS HERE
+  else:
+    st.write("The model predicts that you do not heart disease")
 
